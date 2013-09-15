c2a2b478c95007a1dbdd29e30cca8a1513a49fbe
Mikael Brevik <mikaelbre@gmail.com>
2013-09-15 15:27:53 +0200

    Setting socket.io to use xhr-polling, as Heroku has no WS support

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.use(express.static(__dirname + '/public'));
 
 // Create WS server
 var io = require('socket.io').listen(server);
+
+// Adding support for Socket.IO on Heroku
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 // When a new client connects
 io.sockets.on('connection', function (socket) {
   // Pass on the piano key as a broadcast
