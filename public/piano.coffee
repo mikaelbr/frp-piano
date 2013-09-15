851ce8425249da50a2511e6d2df5b70066b83e98
Mikael Brevik <mikaelbre@gmail.com>
2013-09-15 15:25:13 +0200

    Re-setting socket url to support heroku

--- public/piano.coffee
+++ public/piano.coffee
@@ -3,7 +3,7 @@
 
 # By Mikael Brevik <@mikaelbr>
 
-socket = io.connect 'http://localhost', 8080
+socket = io.connect()
 scale = [
     'A2', 'B2', 'C3', 'D3', 'E3', 'F3', 'G3',
     'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4',
--- public/piano.js
+++ public/piano.js
@@ -2,7 +2,7 @@
 (function() {
   var clicks, keypress, notes, scale, server, socket;
 
-  socket = io.connect('http://localhost', 8080);
+  socket = io.connect();
 
   scale = ['A2', 'B2', 'C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
 
